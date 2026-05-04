0d0013222e3efdf00dc348b4ed2284ff30fbda01
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2026-05-04 20:00:12 +0200

    added axios and integrated api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import React, { useState } from "react";
 import { Search } from "lucide-react";
+import axios from "axios";
 
 export default function Dictionary() {
    let [keyword, setKeyword] = useState("");
+
+   function handleResponse(response) {
+      console.log(response);
+   }
 
    function handleKeywordChange(event) {
       console.log(event.target.value);
@@ -13,6 +18,9 @@ export default function Dictionary() {
    function search(event) {
       event.preventDefault();
       alert(`Searching for ${keyword}`);
+
+      let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=297bdob5643aebcfc422bc019b792eta`;
+      axios.get(apiUrl).then(handleResponse);
    }
 
    return (

86effc30a4292033e55cba0da10928b2454ed2e3
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2026-05-05 22:38:16 +0200

    added some css

--- src/Results.js
+++ src/Results.js
@@ -7,23 +7,25 @@ export default function Results(props) {
    if (props.results && props.results.meanings) {
       return (
          <section className="cards-grid">
-            <article className="learning-card">
+            <article className="learning-card card-primary">
                <div className="word-entry">
                   <h3 className="word-title">{props.results.word}</h3>
                   <p className="phonetic">{props.results.phonetic}</p>
                </div>
             </article>
-            <article className="learning-card">
-               {props.results.meanings.map(function (meaning, index) {
-                  return (
-                     <div key={index}>
-                        <Meaning meaning={meaning} />
-                     </div>
-                  );
-               })}
+            <article className="learning-card card-secondary">
+               {props.results.meanings
+                  .slice(0, 4)
+                  .map(function (meaning, index) {
+                     return (
+                        <div key={index}>
+                           <Meaning meaning={meaning} />
+                        </div>
+                     );
+                  })}
             </article>
-            <article className="learning-card">Card 3</article>
-            <article className="learning-card">Card 4</article>
+            <article className="learning-card card-tertiary">Card 3</article>
+            <article className="learning-card card-quaternary">Card 4</article>
          </section>
       );
    } else {

f8de1a74fc6a378a89679139873a0908fc073898
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2026-05-24 18:28:06 +0200

    corrected styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,7 @@ export default function Synonyms(props) {
    if (props.synonyms) {
       return (
          <div>
-            <h4 className="part-of-speech">Synonyms</h4>
+            <h4 className="synonyms-title">Synonyms</h4>
             <div className="synonyms">
                {props.synonyms.map(function (synonym, index) {
                   return (

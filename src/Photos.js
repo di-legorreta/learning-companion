91975538589be975066393a1442970d01d522022
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2026-06-20 00:04:07 +0200

    added Photos component

---
+++ src/Photos.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { createClient } from "pexels";
+
+export default function Photos(props) {
+   const [photos, setPhotos] = useState([]);
+   useEffect(() => {
+      const client = createClient(
+         "590JfJH2EMMTK9lcw7oJk92iOsHVAbr2VDi1ZqAlTqzf6QTYf5OCpglA",
+      );
+
+      client.photos
+
+         .search({
+            query: props.word,
+
+            per_page: 9,
+         })
+
+         .then((response) => {
+            setPhotos(response.photos);
+         })
+
+         .catch((error) => {
+            console.log("Pexels error:", error);
+         });
+   }, [props.word]);
+
+   return (
+      <div className="photos">
+         {photos.map(function (photo) {
+            return (
+               <img key={photo.id} src={photo.src.landscape} alt={photo.alt} />
+            );
+         })}
+      </div>
+   );
+}
--- src/Results.js
+++ src/Results.js
@@ -3,6 +3,7 @@ import Meaning from "./Meaning";
 import Synonyms from "./Synonyms";
 import Phonetics from "./Phonetics";
 import Language from "./Language";
+import Photos from "./Photos";
 
 export default function Results(props) {
    if (props.results && props.results.meanings) {
@@ -15,32 +16,35 @@ export default function Results(props) {
             return synonym;
          });
       return (
-         <section className="cards-grid">
-            <article className="learning-card card-primary">
-               <div className="word-entry">
-                  <h3 className="word-title">{props.results.word}</h3>
-                  <p className="phonetic">{props.results.phonetic}</p>
-                  <Phonetics word={props.results.word} />
-               </div>
-            </article>
-            <article className="learning-card card-secondary">
-               {props.results.meanings
-                  .slice(0, 3)
-                  .map(function (meaning, index) {
-                     return (
-                        <div key={index}>
-                           <Meaning meaning={meaning} />
-                        </div>
-                     );
-                  })}
-            </article>
-            <article className="learning-card card-tertiary">
-               <Synonyms synonyms={allSynonyms} />
-            </article>
-            <article className="learning-card card-quaternary">
-               <Language word={props.results.word} />
-            </article>
-         </section>
+         <>
+            <section className="cards-grid">
+               <article className="learning-card card-primary">
+                  <div className="word-entry">
+                     <h3 className="word-title">{props.results.word}</h3>
+                     <p className="phonetic">{props.results.phonetic}</p>
+                     <Phonetics word={props.results.word} />
+                  </div>
+               </article>
+               <article className="learning-card card-secondary">
+                  {props.results.meanings
+                     .slice(0, 3)
+                     .map(function (meaning, index) {
+                        return (
+                           <div key={index}>
+                              <Meaning meaning={meaning} />
+                           </div>
+                        );
+                     })}
+               </article>
+               <article className="learning-card card-tertiary">
+                  <Synonyms synonyms={allSynonyms} />
+               </article>
+               <article className="learning-card card-quaternary">
+                  <Language word={props.results.word} />
+               </article>
+            </section>
+            <Photos word={props.results.word} />
+         </>
       );
    } else {
       return null;
